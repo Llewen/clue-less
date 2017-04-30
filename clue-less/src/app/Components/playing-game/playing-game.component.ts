c7210c12c8c47690a4ccae495e0bdcb1e811b92f
henrymgarrett <henrygarrett@henrys-mbp.attlocal.net>
2017-04-30 19:02:46 -0400

    Nav
    Added the nav bar that can be configured later, and about to try out
    the cards

--- clue-less/src/app/Components/playing-game/playing-game.component.ts
+++ clue-less/src/app/Components/playing-game/playing-game.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-playing-game',
+  selector: 'playing-game',
   templateUrl: './playing-game.component.html',
   styleUrls: ['./playing-game.component.css']
 })
