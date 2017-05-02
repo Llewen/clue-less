86aa8237b7276bb5a57d93d686779dc388a69b52
Llewen <ignamor92@gmail.com>
2017-05-01 21:50:20 -0400

    more verbose server

--- server/gameServer/index.ts
+++ server/gameServer/index.ts
@@ -9,27 +9,31 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log("new user");
+  console.log("new user. Socket ID: " + socket.id.toString());
 
   socket.on('chat message', function(msg){
-    console.log("chat message: " + msg);
+    console.log("chat message: " + JSON.stringify(msg));
     io.emit('chat message', msg);
   });
 
   socket.on('game message', function(msg){
-    console.log("game message: " + msg);
+    console.log("game message: " + JSON.stringify(msg));
     io.emit('game message', msg);
   });
 
   socket.on('lobby message', function(msg){
-    console.log("lobby message: " + msg);
+    console.log("lobby message: " + JSON.stringify(msg));
     io.emit('lobby message', msg);
   });
 
   socket.on('user message', function(msg){
-    console.log("user message: " + msg);
+    console.log("user message: " + JSON.stringify(msg));
     io.emit('user message', msg);
-  })
+  });
+
+  socket.on('disconnect', function(){
+    console.log("user disconnected. Socket ID: " + socket.id.toString());
+  });
 });
 
 
